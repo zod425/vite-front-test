2aa9edf8b2d2b214908a9e2f00e4e2a96b8cb4a4
Jang Hyun Seok <hsjang@byulsoft.com>
2022-04-04 16:05:19 +0900

    프론트 소스 수정

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <template>
 
-  <h1>Kafka 테스트</h1>
-  <input type="text" v-model="params.message">
-  <button @click="testKafka">테스트</button>
+  <h1>공통 파라미터</h1>
+  <div>
+    <span>ID : </span>
+    <input type="text" v-model="params.id">
+  </div>
+
+  <div>
+    <span>이름 : </span>
+    <input type="text" v-model="params.name">
+  </div>
+
+  <div>
+    <span>전화 : </span>
+    <input type="text" v-model="params.tel">
+  </div>
+
+
+  <h2>Kafka 테스트</h2>
+  <button @click="testKafka">Kafka 테스트</button>
   <p> kafka 결과 : {{kafkaResult}}</p>
 
-  <h1>Feign 테스트</h1>
-  <input type="text" v-model="params.feignMessage">
-  <button @click="testFiegn">테스트</button>
+  <h2>Feign 테스트</h2>
+  <button @click="testFiegn" :disabled="!feignResult">Feign 테스트 (10초 후 응답)</button>
   <p> feign 결과 : {{feignResult}}</p>
 
-  <h1>Feign 오류 테스트</h1>
-  <input type="text" v-model="params.feignMessage">
-  <button @click="testFiegnFail">테스트</button>
+  <h2>Feign 오류 테스트</h2>
+  <button @click="testFiegnFail">Feign 테스트</button>
   <p> feign 결과 : {{feignFailResult}}</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import exampleService from "@/api/modules/example";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   },
 
   data() {
@@ -38,17 +50,20 @@ export default {
   methods: {
 
     testKafka() {
+      this.kafkaResult = null;
       exampleService.kafka(this.params).then(res => {
         this.kafkaResult = res;
       });
     },
     testFiegn() {
+      this.feignResult = null;
       exampleService.getFeign(this.params).then(res => {
         this.feignResult = res;
       });
     },
 
     testFiegnFail() {
+      this.feignFailResult = null;
       exampleService.setFeign(this.params).then(res => {
         this.feignFailResult = res;
       });
